63f7dc69dfd9039fc3703f1f507812d0da25e8bd
Fred Block <info@fredblock.com>
2015-07-20 22:32:53 -0600

    Add start to deck functionality.
    new file:   css/styles.css
    modified:   index.html
    modified:   lib/deck/index.js
    modified:   lib/index.js
    modified:   package.json
    modified:   test/deck-test.js

--- test/deck-test.js
+++ test/deck-test.js
@@ -1,15 +1,22 @@
 const assert = require('chai').assert;
+var expect = require('chai').expect;
+  // , deck = { hK: 10 };
 const Deck = require('../lib/deck');
 
 describe('creation of deck', function () {
 
   it('exits', function () {
-    console.log("hi!!")
     assert(Deck);
   });
 
-  // it('should construct a deck object', function () {
-  //   assert(new Deck);
-  // });
+  it('should construct a deck object', function () {
+    assert(new Deck());
+  });
+
+  it('should return the correct value for a card', function () {
+    var deck = new Deck();
+
+    expect(deck).to.have.property('hK').to.eql(10);
+  });
 
 });
